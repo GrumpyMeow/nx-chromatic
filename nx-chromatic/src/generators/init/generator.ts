e3c90cf5604123e80068979b953f3d6b251a6191
Sander <schutten@hotmail.com>
2024-10-08 17:18:48 +0200

    wip

--- nx-chromatic/src/generators/init/generator.ts
+++ nx-chromatic/src/generators/init/generator.ts
@@ -1,13 +1,41 @@
 import {
   formatFiles,
   GeneratorCallback,
+  joinPathFragments,
   readNxJson,
+  readProjectConfiguration,
   runTasksInSerial,
   Tree,
   updateNxJson,
 } from '@nx/devkit';
 import { assertNotUsingTsSolutionSetup } from '@nx/js/src/utils/typescript/ts-solution-setup';
 import { InitGeneratorSchema } from './schema';
+import { forEachExecutorOptions } from '@nx/devkit/src/generators/executor-options-utils';
+
+function addChromaticAddon(tree: Tree) {
+    forEachExecutorOptions(
+      tree,
+      '@storybook/angular:build-storybook',
+      (_, projectName, targetName) => {
+        const project = readProjectConfiguration(tree, projectName);
+
+        const filename = joinPathFragments(
+          project.root,
+          '.storybook',
+          'main.ts');
+        const previewTs = tree.read(filename);
+
+        let content = previewTs.toString();
+        if (!content.includes('@chromatic-com/storybook')) {
+          content = content.replace('\'@storybook/addon-essentials\'', '\'@storybook/addon-essentials\', \'@chromatic-com/storybook\'')
+
+          // Todo: probably beter to trigger for all projects: `pnpx storybook add @chromatic-com/storybook --config-dir apps/nxtest/.storybook`
+
+          tree.write(filename,content);
+        }
+      }
+    );
+}
 
 function addCacheableOperation(tree: Tree) {
   const nxJson = readNxJson(tree);
@@ -43,6 +71,7 @@ export async function initGeneratorInternal(tree: Tree, schema: InitGeneratorSch
   schema.addPlugin ??= addPluginDefault;
 
   addCacheableOperation(tree);
+  addChromaticAddon(tree);
 
   const tasks: GeneratorCallback[] = [];
 
